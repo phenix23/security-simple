98d8faf35a368344d7445d43bfa480e3aa37543c
pheonix_23 <hedahdia.faycel@gmail.com>
2022-08-01 18:25:28 +0200

    define user with encoder

--- src/main/java/com/example/securitysimple/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securitysimple/security/ApplicationSecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,4 +32,21 @@ public class ApplicationSecurityConfig {
         return http.build();
     }
 
+    // Add user
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("STUDENT")
+                        .build()
+        );
+    }
+
+    // define password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
