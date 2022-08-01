be66af94d4a70791fe473ebe48cbfead80ee75da
pheonix_23 <hedahdia.faycel@gmail.com>
2022-08-01 13:08:38 +0200

    add basic authentication

---
+++ src/main/java/com/example/securitysimple/security/ApplicationSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.securitysimple.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    // adding basic auth without possibility to logout (every request server need user:password)
+    // WebSecurityConfigureAdapter is deprecated, so we use filterChain in place of Configure Methode
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
